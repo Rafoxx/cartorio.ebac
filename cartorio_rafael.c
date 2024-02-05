4b324a9c435d16ce15624756f55f9b76d37bda44
RafaelGervin <151025392+Rafoxx@users.noreply.github.com>
2024-02-05 06:54:00 -0300

    atualização: opção de saida
    incluso opção de saida do sistema

--- cartorio_rafael.c
+++ cartorio_rafael.c
@@ -3,84 +3,69 @@
 #include <locale.h>
 #include <string.h>
 
-int registro()
-{
+int registro() {
     char arquivo[40];
     char cpf[40];
     char nome[40];
     char sobrenome[40];
     char cargo[40];
 
-    printf("digite o cpf:");
+    printf("Digite o CPF: ");
     scanf("%s", cpf);
 
     strcpy(arquivo, cpf);
     FILE *file;
-    file = fopen(arquivo, "w");
+    file = fopen(arquivo, "a");
+
+    if (file == NULL) {
+        fprintf(stderr, "Não foi possível abrir o arquivo para escrita!\n");
+        return 1;
+    }
+
     fprintf(file, cpf);
-    fclose(file);
-
-    file = fopen(arquivo, "a");
     fprintf(file, ",");
-    fclose(file);
 
-    printf("digite o nome:");
+    printf("Digite o nome: ");
     scanf("%s", nome);
-
-    file = fopen(arquivo, "a");
     fprintf(file, nome);
-    fclose(file);
-
-    file = fopen(arquivo, "a");
     fprintf(file, ",");
-    fclose(file);
 
-    printf("digite o sobrenome:");
+    printf("Digite o sobrenome: ");
     scanf("%s", sobrenome);
-
-    file = fopen(arquivo, "a");
     fprintf(file, sobrenome);
-    fclose(file);
-
-    file = fopen(arquivo, "a");
     fprintf(file, ",");
-    fclose(file);
 
-    printf("digite o cargo:");
+    printf("Digite o cargo: ");
     scanf("%s", cargo);
-
-    file = fopen(arquivo, "a");
     fprintf(file, cargo);
-    fclose(file);
-
-    file = fopen(arquivo, "a");
     fprintf(file, ",");
+
     fclose(file);
 
+    printf("Registro concluído com sucesso!\n");
     system("pause");
+
+    return 0;
 }
 
-int consulta()
-{
+int consulta() {
     setlocale(LC_ALL, "portuguese");
 
     char cpf[40];
     char conteudo[200];
 
-    printf("digite o cpf a ser consultado:");
+    printf("Digite o CPF a ser consultado: ");
     scanf("%s", cpf);
 
     FILE *file;
     file = fopen(cpf, "r");
 
-    if (file == NULL)
-    {
+    if (file == NULL) {
         fprintf(stderr, "Não foi possível abrir o arquivo, não localizado!\n");
-    }
-    else
-    {
-        while (fgets(conteudo, 200, file) != NULL)
-        {
+        system("pause");
+        return 1;
+    } else {
+        while (fgets(conteudo, 200, file) != NULL) {
             printf("\nEssas são as informações do usuário: ");
             printf("%s", conteudo);
             printf("\n\n");
@@ -89,21 +74,18 @@ int consulta()
     }
 
     system("pause");
+    return 0;
 }
 
- int deletar()
-{
+int deletar() {
     char cpf[40];
 
-    printf("digite o cpf a ser deletado:");
+    printf("Digite o CPF a ser deletado: ");
     scanf("%s", cpf);
 
-    if (remove(cpf) == 0)
-    {
+    if (remove(cpf) == 0) {
         printf("Usuário deletado com sucesso!\n");
-    }
-    else
-    {
+    } else {
         printf("Usuário não encontrado!\n");
     }
 
@@ -111,52 +93,64 @@ int consulta()
     return 0;
 }
 
-    
-
-
-int main()
-{
-
+int main() {
     int opcao = 0;
     int laco = 1;
+    char digitarsenha[10] = "a";
+    int comparacao;
 
-    for (laco = 1; laco == 1;)
-    {
+    printf("### Cartório da EBAC ###\n\n");
+    printf("Login de administrador!\n\nDigite sua senha: ");
+    scanf("%s", digitarsenha);
 
-        system("cls");
-        setlocale(LC_ALL, "portuguese");
+    comparacao = strcmp(digitarsenha, "admin");
 
-        printf("### cartório da ebac ###\n\n");
-        printf("Escolha uma opção:\n\n");
-        printf("\t1 - Registrar nomes\n\n");
-        printf("\t2 - Consultar nomes\n\n");
-        printf("\t3 - Deletar nomes\n\n");
-        printf("Opção:");
+    if (comparacao == 0) {
+        do {
+            system("cls");
+            setlocale(LC_ALL, "portuguese");
 
-        scanf("%d", &opcao);
-        system("cls");
+            printf("### Cartório da EBAC ###\n\n");
+            printf("Escolha uma opção:\n\n");
+            printf("\t1 - Registrar nomes\n\n");
+            printf("\t2 - Consultar nomes\n\n");
+            printf("\t3 - Deletar nomes\n\n");
+            printf("\t4 - Sair do sistema\n\n");
+            printf("Opção: ");
+            scanf("%d", &opcao);
 
-        switch (opcao)
-        {
-        case 1:
-            registro();
-            break;
+            system("cls");
 
-        case 2:
-            consulta();
-            break;
+            switch (opcao) {
+                case 1:
+                    registro();
+                    break;
 
-        case 3:
-            deletar();
-            break;
+                case 2:
+                    consulta();
+                    break;
 
-        default:
-            printf("Essa opção não existe!\n");
-            system("pause");
-            break;
-        }
+                case 3:
+                    deletar();
+                    break;
+
+                case 4:
+                    printf("Obrigado por utilizar o sistema.\n");
+                    return 0;
+
+                default:
+                    printf("Essa opção não existe!\n");
+                    system("pause");
+                    break;
+            }
+        } while (laco == 1);
+    } else {
+        printf("Senha incorreta!\n");
     }
+
+    return 0;
 }
+
 
 
 
